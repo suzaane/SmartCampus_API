85453a8a2f7e6a86f0cfdedb1d25fabb07e9e675
suzaane <your-email@example.com>
2026-04-26 21:54:51 +0545

    updated file

--- src/main/java/com/smartcampus/ApplicationConfig.java
+++ src/main/java/com/smartcampus/ApplicationConfig.java
@@ -1,5 +1,17 @@
 package com.smartcampus;
 
-public class ApplicationConfig {
-    
+import org.glassfish.jersey.jackson.JacksonFeature;
+import org.glassfish.jersey.server.ResourceConfig;
+import javax.ws.rs.ApplicationPath;
+
+@ApplicationPath("/api/v1")          // This creates the /api/v1 context path
+public class ApplicationConfig extends ResourceConfig {
+    public ApplicationConfig() {
+        // Scan the same packages your old Main was scanning
+        packages("com.smartcampus.resource",
+                 "com.smartcampus.exception.mapper",
+                 "com.smartcampus.filter");
+        // Enable JSON conversion
+        register(JacksonFeature.class);
+    }
 }
